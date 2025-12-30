2630e83c557ec00158e7a76b0265d5b60b90a4a1
ifflagged <ifflagged@users.noreply.github.com>
2025-12-30 14:49:23 +0000

    Synched sooyaaabo Modules at 2025-12-30 22:49:23 (UTC+8)

--- Modules/JS/sooyaaabo/MyBlockAds/header.js
+++ Modules/JS/sooyaaabo/MyBlockAds/header.js
@@ -1,98 +1,90 @@
-// 2024-08-11 13:40
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en-US"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en-US"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+<head>
+<title>Attention Required! | Cloudflare</title>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<meta name="robots" content="noindex, nofollow" />
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<link rel="stylesheet" id="cf_styles-css" href="/cdn-cgi/styles/cf.errors.css" />
+<!--[if lt IE 9]><link rel="stylesheet" id='cf_styles-ie-css' href="/cdn-cgi/styles/cf.errors.ie.css" /><![endif]-->
+<style>body{margin:0;padding:0}</style>
 
-const url = $request.url;
-const header = $request.headers;
-const contype = header["Content-Type"] || header["content-type"];
-const headopt = header["Operation-Type"] || header["operation-type"];
-const ua = header["User-Agent"] || header["user-agent"];
-const isLoon = typeof $task !== "undefined";
 
-if (url.includes("/amdc/mobileDispatch")) {
-  if (
-    ua.includes("AMapiPhone") || // 高德地图
-    ua.includes("Alibaba") || // 阿里巴巴
-    ua.includes("Cainiao4iPhone") || // 菜鸟
-    ua.includes("%E9%97%B2%E9%B1%BC") || // 闲鱼
-    ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") || // 飞猪旅行
-    ua.includes("%E5%96%B5%E8%A1%97") ||
-    ua.includes("%E5%A4%A9%E7%8C%AB") ||
-    ua.includes("Moon") ||
-    ua.includes("DMPortal")
-  ) {
-    if (isLoon) {
-      $done({ status: "HTTP/1.1 404 Not Found" });
-    } else {
-      $done();
-    }
-  } else {
-    $done({});
+<!--[if gte IE 10]><!-->
+<script>
+  if (!navigator.cookieEnabled) {
+    window.addEventListener('DOMContentLoaded', function () {
+      var cookieEl = document.getElementById('cookie-alert');
+      cookieEl.style.display = 'block';
+    })
   }
-} else if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
-  // 12306页面内容
-  const list12306 = [
-    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
-    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
-    // "com.cars.otsmobile.city",
-    // "com.cars.otsmobile.initCountry",
-    // "com.cars.otsmobile.initNewSysCache",
-    // "com.cars.otsmobile.initProvince",
-    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
-    "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
-    // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
-    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
-    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
-    "com.cars.otsmobile.newHomePageBussData", // 商品信息流
-    // "com.cars.otsmobile.newHomePageRefresh",
-    "com.cars.otsmobile.paySuccBuss.bussEntryShow" // 商业推广
-    // "com.cars.otsmobile.travelPage.initData", // 出行服务
-  ];
-  if (isLoon) {
-    if (list12306?.includes(headopt)) {
-      $done({ status: "HTTP/1.1 404 Not Found" });
-    } else {
-      $done({});
-    }
-  } else {
-    if (list12306?.includes(headopt)) {
-      $done();
-    } else {
-      $done({});
-    }
-  }
-} else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
-  // 中国农业银行开屏广告
-  const listbankabc = [
-    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
-    "com.abchina.mbank.common.homepage.getStartParam"
-  ];
-  if (isLoon) {
-    if (listbankabc?.includes(headopt)) {
-      $done({ status: "HTTP/1.1 404 Not Found" });
-    } else {
-      $done({});
-    }
-  } else {
-    if (listbankabc?.includes(headopt)) {
-      $done();
-    } else {
-      $done({});
-    }
-  }
-} else if (url.includes("/sec.sginput.qq.com/q")) {
-  // 搜狗输入法候选词推广
-  if (isLoon) {
-    if (contype === "application/octet-stream") {
-      $done({ status: "HTTP/1.1 404 Not Found" });
-    } else {
-      $done({});
-    }
-  } else {
-    if (contype === "application/octet-stream") {
-      $done();
-    } else {
-      $done({});
-    }
-  }
-} else {
-  $done({});
-}
+</script>
+<!--<![endif]-->
+
+</head>
+<body>
+  <div id="cf-wrapper">
+    <div class="cf-alert cf-alert-error cf-cookie-error" id="cookie-alert" data-translate="enable_cookies">Please enable cookies.</div>
+    <div id="cf-error-details" class="cf-error-details-wrapper">
+      <div class="cf-wrapper cf-header cf-error-overview">
+        <h1 data-translate="block_headline">Sorry, you have been blocked</h1>
+        <h2 class="cf-subheadline"><span data-translate="unable_to_access">You are unable to access</span> 103516.xyz</h2>
+      </div><!-- /.header -->
+
+      <div class="cf-section cf-highlight">
+        <div class="cf-wrapper">
+          <div class="cf-screenshot-container cf-screenshot-full">
+            
+              <span class="cf-no-screenshot error"></span>
+            
+          </div>
+        </div>
+      </div><!-- /.captcha-container -->
+
+      <div class="cf-section cf-wrapper">
+        <div class="cf-columns two">
+          <div class="cf-column">
+            <h2 data-translate="blocked_why_headline">Why have I been blocked?</h2>
+
+            <p data-translate="blocked_why_detail">This website is using a security service to protect itself from online attacks. The action you just performed triggered the security solution. There are several actions that could trigger this block including submitting a certain word or phrase, a SQL command or malformed data.</p>
+          </div>
+
+          <div class="cf-column">
+            <h2 data-translate="blocked_resolve_headline">What can I do to resolve this?</h2>
+
+            <p data-translate="blocked_resolve_detail">You can email the site owner to let them know you were blocked. Please include what you were doing when this page came up and the Cloudflare Ray ID found at the bottom of this page.</p>
+          </div>
+        </div>
+      </div><!-- /.section -->
+
+      <div class="cf-error-footer cf-wrapper w-240 lg:w-full py-10 sm:py-4 sm:px-8 mx-auto text-center sm:text-left border-solid border-0 border-t border-gray-300">
+    <p class="text-13">
+      <span class="cf-footer-item sm:block sm:mb-1">Cloudflare Ray ID: <strong class="font-semibold">9b62571b0ba32522</strong></span>
+      <span class="cf-footer-separator sm:hidden">&bull;</span>
+      <span id="cf-footer-item-ip" class="cf-footer-item hidden sm:block sm:mb-1">
+        Your IP:
+        <button type="button" id="cf-footer-ip-reveal" class="cf-footer-ip-reveal-btn">Click to reveal</button>
+        <span class="hidden" id="cf-footer-ip">52.159.228.209</span>
+        <span class="cf-footer-separator sm:hidden">&bull;</span>
+      </span>
+      <span class="cf-footer-item sm:block sm:mb-1"><span>Performance &amp; security by</span> <a rel="noopener noreferrer" href="https://www.cloudflare.com/5xx-error-landing" id="brand_link" target="_blank">Cloudflare</a></span>
+      
+    </p>
+    <script>(function(){function d(){var b=a.getElementById("cf-footer-item-ip"),c=a.getElementById("cf-footer-ip-reveal");b&&"classList"in b&&(b.classList.remove("hidden"),c.addEventListener("click",function(){c.classList.add("hidden");a.getElementById("cf-footer-ip").classList.remove("hidden")}))}var a=document;document.addEventListener&&a.addEventListener("DOMContentLoaded",d)})();</script>
+  </div><!-- /.error-footer -->
+
+    </div><!-- /#cf-error-details -->
+  </div><!-- /#cf-wrapper -->
+
+  <script>
+    window._cf_translation = {};
+    
+    
+  </script>
+</body>
+</html>
