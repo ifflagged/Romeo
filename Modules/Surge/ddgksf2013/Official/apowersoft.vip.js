eb3ec8305a3d2f8625a40a706fd190f1d612dc35
ifflagged <ifflagged@users.noreply.github.com>
2025-12-31 14:01:27 +0000

    Synched ddgksf2013 Modules at 2025-12-31 22:01:27 (UTC+8)

---
+++ Modules/Surge/ddgksf2013/Official/apowersoft.vip.js
@@ -0,0 +1,22 @@
+/***********************************
+
+傲软抠图 Pro
+
+https://t.me/ddgksf2021
+
+[rewrite_local]
+
+
+^https?:\/\/.*aoscdn\.com\/base\/vip\/v\d\/vips url script-response-body https://ddgksf2013.top/scripts/apowersoft.vip.js
+
+[mitm] 
+
+hostname=*.aoscdn.com
+
+***********************************/
+
+
+
+
+
+var ddgksf2013={status:200,message:"success",data:{group_expired_at:0,is_tried:0,max_devices:1,period_type:"active",candy_expired_at:0,pending:0,remained_seconds:0,limit:0,expired_at:4045798296,candy:0,license_type:"premium",quota:0,status:1,coin:100}};$done({body:JSON.stringify(ddgksf2013)});
