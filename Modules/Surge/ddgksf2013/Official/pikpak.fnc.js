caa481738839670a432c9349305bcea5a4218330
ifflagged <ifflagged@users.noreply.github.com>
2026-01-02 16:40:57 +0000

    Synched ddgksf2013 Modules at 2026-01-03 00:40:56 (UTC+8)

---
+++ Modules/Surge/ddgksf2013/Official/pikpak.fnc.js
@@ -0,0 +1,15 @@
+/*************************
+
+解决pikpak中国大陆无法使用问题
+
+[rewrite_local]
+
+^https?:\/\/access\.mypikpak\.com\/access_controller url reject-200
+
+
+[mitm] 
+
+hostname = access.mypikpak.com
+
+
+*************************/
