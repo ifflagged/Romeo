0ef5bdc4d1c3298996115f117661e628a31e5b79
ifflagged <ifflagged@users.noreply.github.com>
2026-01-02 14:24:24 +0000

    Synched ddgksf2013 Modules at 2026-01-02 22:24:24 (UTC+8)

--- Modules/Surge/ddgksf2013/Official/javdbapp.ads.js
+++ Modules/Surge/ddgksf2013/Official/javdbapp.ads.js
@@ -8,11 +8,11 @@ JAVDB IOS APP 去广告
 [rewrite_local]
 
 # > JavDB_开屏广告
-^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1,3}(:\d+)?\/api\/v\d\/startup  url jsonjq-response-body 'del(.data.settings.NOTICE)| .data.splash_ad.enabled = false| .data.splash_ad.overtime = 0'
+^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1,3}(:\d+)?\/api\/v\d\/startup url script-response-body https://ddgksf2013.top/scripts/javdbapp.ads.js
 # > JavDB_Tab广告
-^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1,3}(:\d+)?\/api\/v\d\/ads  url jsonjq-response-body '.data.ads = null'
+^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1,3}(:\d+)?\/api\/v\d\/ads url script-response-body https://ddgksf2013.top/scripts/javdbapp.ads.js
 # > JavDB_播放页
-^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1,3}(:\d+)?\/api\/v4\/movies  url jsonjq-response-body '.data.show_vip_banner = false'
+^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1,3}(:\d+)?\/api\/v4\/movies url script-response-body https://ddgksf2013.top/scripts/javdbapp.ads.js
 
 
 [mitm] 
@@ -22,4 +22,36 @@ hostname = api.pxxgg.xyz, api.ujvnmkx.cn, jdforrepam.com, api.yijingluowangluo.x
 **********/
 
 
-$done();
+let body = $response.body;
+let url = $request.url;
+let obj = JSON.parse(body);
+
+try {
+  // 开屏广告
+  if (/\/api\/v\d\/startup/.test(url)) {
+    if (obj?.data?.settings?.NOTICE) {
+      delete obj.data.settings.NOTICE;
+    }
+    if (obj?.data?.splash_ad) {
+      obj.data.splash_ad.enabled = false;
+      obj.data.splash_ad.overtime = 0;
+    }
+  }
+
+  // Tab 广告
+  if (/\/api\/v\d\/ads/.test(url)) {
+    if (obj?.data) {
+      obj.data.ads = null;
+    }
+  }
+
+  // 播放页 VIP Banner
+  if (/\/api\/v4\/movies/.test(url)) {
+    if (obj?.data) {
+      obj.data.show_vip_banner = false;
+    }
+  }
+} catch (e) {}
+
+$done({ body: JSON.stringify(obj) });
+
