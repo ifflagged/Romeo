874cb0378b3ddce934441a330a607f08e79ede27
ifflagged <ifflagged@users.noreply.github.com>
2026-03-13 14:55:06 +0000

    Synched ddgksf2013 Modules at 2026-03-13 22:55:06 (UTC+8)

--- Modules/Surge/ddgksf2013/Official/buyitunes.vip.js
+++ Modules/Surge/ddgksf2013/Official/buyitunes.vip.js
@@ -69,8 +69,8 @@ var productidmap={
 	'org.zrey.money':['costmemo','org.zrey.money.lifetime',1],
 	'com.sfun.snapedit':['weikeMusic','com.moiseum.dailyart.subscription.Patron',0],
 	'com.polygitapp.polygit':['polygit','com.polygitapp.polygit.pro.yearly',0],
-	'com.sm.widget':['colorwidget','com.sm.widget.Pro',0]
-	
+	'com.sm.widget':['colorwidget','com.sm.widget.Pro',0],
+	'linsen.camera.app':['shiguangxiangji','linsen.camera.App.yearly',0]
 }
 
 // ========= 固定部分 ========= //
