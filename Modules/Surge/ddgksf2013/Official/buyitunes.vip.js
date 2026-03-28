c898c92cb5c5520de97d2440329b4d3f9bd5a82d
ifflagged <ifflagged@users.noreply.github.com>
2026-03-28 07:10:50 +0000

    Synched ddgksf2013 Modules at 2026-03-28 15:10:50 (UTC+8)

--- Modules/Surge/ddgksf2013/Official/buyitunes.vip.js
+++ Modules/Surge/ddgksf2013/Official/buyitunes.vip.js
@@ -71,7 +71,7 @@ var productidmap={
 	'com.polygitapp.polygit':['polygit','com.polygitapp.polygit.pro.yearly',0],
 	'com.sm.widget':['colorwidget','com.sm.widget.Pro',0],
 	'linsen.camera.app':['shiguangxiangji','linsen.camera.App.yearly',0],
-	'com.moiseum.dailyart':['dailyart','com.moiseum.dailyart.subscription.Patron',0]
+	'com.moiseum.dailyart':['dailyart','com.moiseum.dailyart.subscription.monthly',0]
 }
 
 // ========= 固定部分 ========= //
