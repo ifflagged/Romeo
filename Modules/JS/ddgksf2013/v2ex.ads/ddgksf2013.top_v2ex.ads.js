619b2124559351cbd72d95d95747c0368e8ab7c3
ifflagged <ifflagged@users.noreply.github.com>
2026-07-07 12:06:19 +0000

    Synched ddgksf2013 Modules at 2026-07-07 20:06:18 (UTC+8)

--- Modules/JS/ddgksf2013/v2ex.ads/ddgksf2013.top_v2ex.ads.js
+++ Modules/JS/ddgksf2013/v2ex.ads/ddgksf2013.top_v2ex.ads.js
@@ -2,7 +2,7 @@
 
 墨鱼自用V2EX网页去广告
 
-2026-01-30
+2026-07-07
 
 https://t.me/ddgksf2021
 
@@ -11,6 +11,7 @@ https://t.me/ddgksf2021
 
 ^https?:\/\/.*v2ex\.com\/(?!(.*(api|login|cdn-cgi|verify|auth|captch|(\.(js|css|jpg|jpeg|png|webp|gif|zip|woff|woff2|m3u8|mp4|mov|m4v|avi|mkv|flv|rmvb|wmv|rm|asf|asx|mp3|json|ico|otf|ttf))))) url script-response-body https://ddgksf2013.top/scripts/v2ex.ads.js
 ^https?:\/\/pagead.*googlesyndication\.com\/pagead url reject-200
+^https?:\/\/.*v2ex\.com\/pro\/emitter url reject-200
 
 [mitm] 
 
