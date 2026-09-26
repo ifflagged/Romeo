dd29f54a000b9674c8a9fc528d412e8982b228cf
ifflagged <ifflagged@users.noreply.github.com>
2026-09-26 07:32:13 +0000

    Synched ddgksf2013 Modules at 2026-09-26 15:32:13 (UTC+8)

---
+++ Modules/JavaScript/ddgksf2013/Official/vistopia.vip.js
@@ -0,0 +1,17 @@
+/*
+Vistopia Pro
+
+2026.09.26V1.0.7
+
+https://t.me/ddgksf2021
+
+[rewrite_local]
+
+^https?:\/\/api\.vistopia\.com\.cn\/api\/v\d+\/(user\/profile|content\/(article_list|content-show)|reader\/section-detail|services\/play-token) url script-response-body https://raw.githubusercontent.com/ddgksf2013/Scripts/refs/heads/master/vistopia.js
+
+[mitm]
+
+hostname = api.vistopia.com.cn
+
+*/
+
